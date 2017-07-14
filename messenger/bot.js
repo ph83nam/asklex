a0c61d23a2e985e7c85fba986beb7700cb2010f3
Nam Pham <ph83nam@gmail.com>
2017-07-14 22:12:47 +0700

    message subscribe and hander skeleton

---
+++ messenger/bot.js
@@ -0,0 +1,173 @@
+const PAGE_ACCESS_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN;
+const VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
+const API_VERSION = process.env.FB_API_VERSION || '2.9';
+const extend = require('extend');
+const request = require('request');
+
+/**
+ * http request
+ * @param {string} path
+ * @param {object} data
+ * @param {function} callback
+ * @param {string} method
+ */
+function callGraphApi(path, data, callback, method) {
+  const params = { access_token: PAGE_ACCESS_TOKEN };
+  const opts = {
+    uri: `https://graph.facebook.com/v${API_VERSION}${path}`,
+    qs: params,
+    method: method || 'GET',
+    headers: {
+      accept: 'application/json',
+    },
+  };
+  if (!method || method === 'GET') {
+    if (data) extend(params, data);
+  } else {
+    opts.json = data;
+  }
+  // callback wrapper
+  const cb = function (error, success) {
+    let err = error;
+    let resp = success;
+    if (error === null && success) {
+      // check if success is a JSON object
+      if (typeof resp.body === 'string') {
+        if (resp.headers['content-type'].indexOf('application/json') === 0) {
+          try {
+            resp = JSON.parse(resp.body);
+          } catch (ex) {
+            err = `failed to parse body ${ex.message}`;
+          }
+        }
+      } else if (typeof resp.body === 'object') {
+        if (!resp.body.error) {
+          resp = resp.body;
+        } else {
+          err = resp.body.error;
+        }
+      }
+    }
+    if (callback) callback(err, resp);
+  };
+  return request(opts, cb);
+}
+
+/**
+ * default callback
+ * @param {Error} error
+ * @param {Response} response
+ * @param {object} body
+ */
+function sendMessageCb(error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const recipientId = body.recipient_id;
+    const messageId = body.message_id;
+
+    console.log('Successfully sent generic message with id %s to recipient %s',
+      messageId, recipientId);
+  } else {
+    console.error('Unable to send message.');
+    console.error(response);
+    console.error(error);
+  }
+}
+
+/**
+ * send a message to facebook
+ * @param {object} messageData
+ * @param {function} callback
+ */
+function sendMessage(messageData, callback) {
+  return callGraphApi('/me/messages', messageData, callback || sendMessageCb, 'POST');
+}
+
+/**
+ * get user profile information
+ * @param {string} userId
+ * @param {function} callback
+ * @return {object}
+ */
+function getUserProfile(userId, callback) {
+  const params = {
+    fields: 'first_name,last_name,locale,timezone,gender',
+  };
+  return callGraphApi(`/${userId}`, params, callback, 'GET');
+}
+
+/**
+ * return http response with text
+ * @param {function} callback
+ * @param {string} text
+ */
+function respondWithText(callback, text) {
+  const resp = {
+    statusCode: 200,
+    headers: {},
+    body: text,
+  };
+  callback(null, resp);
+}
+
+/**
+ * page message callback
+ * @param {object} event
+ * @param {context} context
+ * @param {function} callback
+ */
+function onPageMessage(event, context, callback) {
+  // @todo: identify user and session
+
+  // @todo: read text or voice
+
+  // @todo: convert to english if require
+
+  respondWithText(callback, 'received');
+
+  // @todo: invoke Lex postContent
+
+  // @todo: convert to original language (if applicable)
+
+  // @todo: send message
+}
+
+/**
+ * subscription verification message from FB
+ * @param {object} event
+ * @param {object} context
+ * @param {function} callback
+ */
+function onPageSubcribe(event, context, callback) {
+  const params = event.queryStringParameters || {};
+  // verify token
+  if (params['hub.verify_token'] !== VERIFY_TOKEN) {
+    callback(null, {
+      statusCode: 403,
+      body: 'invalid verify_token',
+    });
+  } else {
+    respondWithText(callback, params['hub.challenge'] || 'missing challenge');
+  }
+}
+
+/**
+ * AWS lamda callback
+ * @param {object} event
+ * @param {context} context
+ * @param {function} callback
+ */
+function onMessage(event, context, callback) {
+  console.log('message', event, context, callback);
+  const params = event.queryStringParameters || {};
+  // handle webhook mode
+  if (params['hub.mode'] === 'subscribe') {
+    onPageSubcribe(event, context, callback);
+  } else {
+    onPageMessage(event, context, callback);
+  }
+}
+
+exports.message = onMessage;
+exports.callGraphApi = callGraphApi;
+exports.sendMessage = sendMessage;
+exports.getUserProfile = getUserProfile;
---
+++ test/messenger/bot.js
@@ -0,0 +1,59 @@
+import { expect } from 'chai';
+import * as bot from '../../messenger/bot';
+
+const FB_USER_ID = process.env.FB_USER_ID;
+const FB_VERIFY_TOKEN = process.env.FB_VERIFY_TOKEN;
+
+describe('bot', () => {
+  it('#getUserProfile', (done) => {
+    bot.getUserProfile(FB_USER_ID, (error, success) => {
+      expect(error).to.be.null;
+      expect(success).to.haveOwnProperty('first_name');
+      done();
+    });
+  });
+
+  it('#sendMessage', (done) => {
+    const msg = {
+      recipient: {
+        id: FB_USER_ID,
+      },
+      message: {
+        text: 'hello, I\'m Mocha!',
+      },
+    };
+    bot.sendMessage(msg, (error, success) => {
+      expect(error).to.be.null;
+      expect(success.message_id).not.to.be.undefined;
+      done();
+    });
+  });
+
+  it('#messsage: 403', () => {
+    const event = {
+      queryStringParameters: {
+        'hub.mode': 'subscribe',
+      },
+    };
+    const context = {};
+    bot.message(event, context, (error, response) => {
+      expect(error).to.be.null;
+      expect(response.statusCode).eq(403);
+    });
+  });
+
+  it('#messsage:subscribe', () => {
+    const event = {
+      queryStringParameters: {
+        'hub.mode': 'subscribe',
+        'hub.challenge': '1234567890',
+        'hub.verify_token': FB_VERIFY_TOKEN,
+      },
+    };
+    const context = {};
+    bot.message(event, context, (error, response) => {
+      expect(error).to.be.null;
+      expect(response.body).to.equal('1234567890');
+    });
+  });
+});
